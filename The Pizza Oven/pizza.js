7d65cee15176401bfef0b26d0240de263f999fe1
JadeAtwood <jadeatwood@protonmail.com>
2021-05-25 21:01:00 -0400

    mid way debugging

---
+++ The Pizza Oven/pizza.js
@@ -0,0 +1,27 @@
+PizzaOven(){
+    var crust = ["deep dish", 'traditional', "thin"];
+    var sauce = ["marinara", "white", "traditional"];
+    var cheese = ["mozzerrella", "feta"]
+    var toppings = ["pepperon", "olives", "onions", "mushrooms", "sausage"];
+    var pizza{}
+        console.log("the crust is " + crust)
+        console.log("the sauce is " + sauce)
+        console.log("the cheese is " +cheese)
+        console.log("the crust is " + crust)
+        return pizza;
+            
+var Pizza1 = PizzaOven(["deep dish"], ["marinara"], ["mozzerrella"]);
+    console.log(Pizza1);
+
+        // function sandwichFactory(bread, protein, cheese, toppings) {
+        //     var sandwich = {};
+        //     sandwich.bread = bread;
+        //     sandwich.protein = protein;
+        //     sandwich.cheese = cheese;
+        //     sandwich.toppings = toppings;
+        //     return sandwich;
+        // }
+            
+        // var s1 = sandwichFactory("wheat", "turkey", "provolone", ["mustard", "fried onions", "arugula"]);
+        // console.log(s1);
+                
